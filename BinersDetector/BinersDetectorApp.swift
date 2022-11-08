ec42e423f472d6896c6f093e88e8c2b21fc46532
matarangavlad <vlad.mataranga@garmin.com>
2022-11-08 21:24:33 +0200

    Initial Commit

---
+++ BinersDetector/BinersDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BinersDetectorApp.swift
+//  BinersDetector
+//
+//  Created by Mataranga, Vlad on 08.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct BinersDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
